1310556fe3b791f8e8c3ac289ab55fd7a2747022
HQuang0910 <134147330+ItsAzura@users.noreply.github.com>
2025-02-18 21:21:10 +0700

    Create and Register ApplicationDbContext

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using learn_crud.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace learn_crud
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            //Đăng ký ApplicationDbContext vào Dependency Injection (DI) để có thể sử dụng nó trong ứng dụng.
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                //Lấy chuỗi kết nối từ appsettings.json
+                var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+                //Sử dụng SQL Server làm database
+                options.UseSqlServer(connectionString);
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
---
+++ Services/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace learn_crud.Services
+{
+    //ApplicationDbContext giúp Asp.net core giao tiếp với database thông qua Entity Framework Core
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+       
+    }
+}
